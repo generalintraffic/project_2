2d7524fa21845be705267c9ab489da6949bdb16b
jtorrealba3200 <jtorrealba3@gmail.com>
2016-07-19 17:41:58 -0400

    para trabajar el javascrip

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiamF2aWVyMyIsImEiOiJjaXFzcDYzZHowMnhnZm5ubndxdWo0anJoIn0.3ANHMEByhAl7OFnMlwwSrA';
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([38.9, -77], 12);
+
+

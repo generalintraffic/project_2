2df87a73769dc608b1d7694667f8aa563817bc93
Jennyree <jaar1909@gmail.com>
2016-07-19 20:07:28 -0400

    #126545945 Punto Origen

--- js/main.js
+++ js/main.js
@@ -3,4 +3,58 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiamF2aWVyMyIsImEiOiJjaXFzcDYzZHowMnhnZm5ubndxd
 var map = L.mapbox.map('map', 'mapbox.streets')
     .setView([38.9, -77], 12);
 
+init();
+var fixedMarker, count = true;
+
+function init(){
+	
+	map.on('click', function(ev) {
+		var coord= [];
+			if (count == true) {
+				coord = [ev.latlng.lat, ev.latlng.lng];
+				console.log(coord);
+				coordinates(coord);
+				count = false;
+			}
+			else if ( count == false ) {
+				map.removeLayer(fixedMarker)
+				coord = [ev.latlng.lat, ev.latlng.lng];
+				coordinates(coord);
+				console.log(coord);
+				
+			}
+	});
+};
+
+function coordinates(coord){
+	var coord = coord;
+	console.log('coord ' + coord);
+	fixedMarker = L.marker(new L.LatLng(coord[0],coord[1]), {
+		icon: L.mapbox.marker.icon({
+		  'marker-color': 'ff8888'
+		})
+	}).bindPopup('Mapbox DC').addTo(map);
+	return fixedMarker;
+};
+
+function myFunction() {
+	var x;
+	if (confirm("Do you wish to continue?") == true) {
+		text();
+	
+	} else {
+		init();
+
+	}
+
+}
+
+function text(){
+	console.log(fixedMarker);
+	var fc = fixedMarker.getLatLng();
+	// Create a featureLayer that will hold a marker and linestring.
+	var featureLayer = L.mapbox.featureLayer().addTo(map);
+	console.log(fc);
+	end(fc);
+};
 

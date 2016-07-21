75beb043cee104861fecf318689a6a47c6db4ed0
jtorrealba3200 <jtorrealba3@gmail.com>
2016-07-21 16:41:31 -0400

    repo base con react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.scss';
+
+
+import App from './components/app/App.js';
+ReactDOM.render(<App/>, document.getElementById('map'));
